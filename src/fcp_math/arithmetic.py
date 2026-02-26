8564c8b22da8d3a3ea11c267723c00fdb9f6699f
leesugil <sugilmath@gmail.com>
2026-02-25 19:05:55 -0500

    crucial update: fixed the unintended behavior of 2fcpsec functions. now works fine with all kinds of fps

--- src/fcp_math/arithmetic.py
+++ src/fcp_math/arithmetic.py
@@ -13,6 +13,36 @@ def unfrac(frac):
     output = Fraction(num, denom)
     return output.numerator, output.denominator
 
+def to_denom(frac, denom):
+    """
+    frac: p/q
+    denom: D
+    solution: n/D
+    n ~ pD/q
+    n = round(pD/q)
+    solution = round(pD/q)/D
+    """
+    p = frac.numerdator
+    q = frac.denominator
+    D = denom
+    n = round(p*D/q)
+    return Fraction(n, D)
+
+def to_fps(frac, fps):
+    """
+    frac = a/b
+    fps = p/q
+    frac ~ np/q
+    n ~ aq/bp
+    n = round(aq/bp)
+    solution = round(aq/bp)*p/q
+    """
+    p, q = unfrac(fps)
+    a = frac.numerator
+    b = frac.denominator
+    n = round(a*q/(b*p))
+    return Fraction(n*p, q)
+
 # DEPRECIATED. NEVER USE IT UNLESS YOU DON'T MIND INACCURACY. RESULTS DRIFT.
 def fcpsec2float(text: str, fps: str='100/6000s') -> float:
     """
@@ -40,9 +70,8 @@ def float2fcpsec(x: float, fps: str='100/6000s') -> str:
 
     output: aaaaaa/bbs
     """
-    num, denom = unfrac(fps) # already reduced, num better be 1.
-    assert num == 1
-    frac = Fraction(x).limit_denominator(denom)
+    frac = Fraction(x)
+    frac = to_fps(frac, fps)
     return f"{frac.numerator}/{frac.denominator}s"
 
 def fcpsec2frac(text: str):
@@ -50,9 +79,13 @@ def fcpsec2frac(text: str):
         text = text[:-1] + '/1s'
     return Fraction(text[:-1])
 
-def frac2fcpsec(frac, fps='100/6000s'):
+def frac2fcpsec(frac, fps='100/6000s', debug=False):
     num, denom = unfrac(fps)
-    frac = frac.limit_denominator(denom)
+    if debug:
+        print(f"frac2fcpsec. frac: {frac}, desired_denominator: {denom}")
+    frac = to_fps(frac, fps)
+    if debug:
+        print(f"frac2fcpsec. modified_frac: {frac}")
     return f"{frac.numerator}/{frac.denominator}s"
 
 def fcpsec_add(a: str, b: str, fps: str='100/6000s') -> str:
